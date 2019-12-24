801d7d56369318f36244e05d6b5c77f3efd76080
ferditezcan <tzcnferdi@gmail.com>
2019-12-24 15:49:20 +0200

    eshopapp

---
+++ E-ShopApp/README.md
@@ -0,0 +1,34 @@
+# ReactJS-Basics
+The quick guide to learn basic concepts and workflow of how to build React app
+Description
+
+This short course will cover basic and core concepts that you need to know to get up and running with ReactJS.
+
+React JS is a Javascript library for building user interfaces. It's flexible, fast, easy to learn and fun to work with. ReactJS is designed to make the process of building modular, reusable user interface components simple and intuitive. React was developed at Facebook and focuses on the 'View' aspect of MVC in web applications. React was built for the purpose of developing applications that are large in nature and have to deal with time changing data.
+
+Also in this course, it will get you up to speed quickly with easy to follow short videos. It is project-oriented with hands-on examples, from simple to complex, that will help you with the most fundamentals concepts of React:
+
+React custom syntax and the JSX transform
+components, properties and state
+The power of the virtual DOM
+Topics include:
+
+Compiling JSX to plain JS
+introduction to Npm react-tools
+Creating components
+Using and populating properties
+Adding and using state
+Handling events
+
+
+What you’ll learn
+Taste of how to build your very first React component.
+How to handle event in components.
+Understand two key concepts of React: State and Props.
+How to reuse and nest components.
+How to transfer data between Parent, Child components.
+How to work with Array, Form
+Are there any course requirements or prerequisites?
+Nothing as you will be working on a online code editor so it's super convenient.
+Who this course is for:
+Anyone who want to learn React without much knowledge of HTML or CSS.
---
+++ E-ShopApp/script.jsx
@@ -0,0 +1,106 @@
+var Product = React.createClass({
+  getInitialState: function() {
+    return{qty: 0};
+    
+  },
+  buy: function() {
+    this.setState({qty: this.state.qty + 1});
+    this.props.handleTotal(this.props.price);
+  },
+  show: function() {
+    this.props.handleShow(this.props.name);
+  },
+  render: function() {
+    return(
+      <div>
+      <p>{this.props.name} - ${this.props.price}</p>
+      <button onClick={this.buy}>Buy</button>
+      <button onClick={this.show}>Show</button>
+      <h3>Qty: {this.state.qty} item(s)</h3>
+      <hr/>
+      </div>
+    );
+  }
+});
+
+var Total = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <h3>Total Cash: ${this.props.total}  </h3>
+        
+      </div>
+    );
+  } 
+});
+
+var ProductForm = React.createClass({
+  submit: function(e){
+    e.preventDefault();
+  
+    var product = {
+      name: this.refs.name.value,
+      price: parseInt(this.refs.price.value)
+    }
+    
+    this.props.handleCreate(product);
+    
+    this.refs.name.value = "";
+    this.refs.price.value = "";
+  },
+  render: function() {
+    return(
+      <form onSubmit={this.submit}>
+        <input type="text" placeholder="Product Name" ref="name"/> 
+        -<input type="text" placeholder="Product Price" ref="price"/>
+        <br/><br/>
+        <button>Create Product</button>
+        <hr/>
+      </form>
+      );
+    }
+  });
+  
+var ProductList = React.createClass({
+  getInitialState: function() {
+    return {
+      total:0,
+      productList:[                      // ARRAY
+        {name: "Adroid",price: 121},
+        {name: "Apple",price: 350},
+        {name: "Nokia",price: 65},
+        ]
+    };
+  },
+  createProduct: function(product){
+    this.setState({
+      productList: this.state.productList.concat(product)
+    })
+  },
+  calculateTotal: function(price){
+    this.setState({total: this.state.total + price});
+  },
+  showProduct: function(name){
+    alert("You selected "+ name)
+  },
+  render: function() {
+    var component = this;
+    var products = this.state.productList.map(function(product){
+    return(
+      <Product name={product.name} price={product.price}
+      handleShow={component.showProduct}
+      handleTotal={component.calculateTotal}/>
+      );
+    });
+    
+    return(
+      <div>
+      <ProductForm handleCreate={this.createProduct}/>
+      {products}
+      <Total total={this.state.total}/>
+      </div>
+      );
+   }
+});
+
+React.render(<ProductList/>, document.getElementById("root"));
